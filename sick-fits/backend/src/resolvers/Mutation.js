a3c2711880513e3470aa84a4bfc9fcfb1cf376ab
danst3in <50937807+danst3in@users.noreply.github.com>
2021-01-18 16:30:15 -0800

    Delete Items, update cache, update db

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -30,6 +30,21 @@ const Mutations = {
     console.log(item);
     return item;
   },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. Find the item - query
+    const item = await ctx.db.query.item(
+      { where },
+      `{
+      id
+      title
+    }`
+    );
+    // 2. Check if they own that item, or have permissions
+    // TODO
+    // 3. Delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "./Items";
+
+export const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default class DeleteItem extends Component {
+  update = (cache, payload) => {
+    //  manually update the cache on the Client, so it matches the server
+    // 1. Read the cache for the items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log("🚀 ~ file: DeleteItem.js ~ line 19 ~ DeleteItem ~ data", data);
+    // 2. Filter the deleted item out of the page cache
+    data.items = data.items.filter(
+      (item) => item.id !== payload.data.deleteItem.id
+    );
+    // 3. Put the items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            onClick={() => {
+              if (confirm("Are you sure you want to delete this item?")) {
+                deleteItem();
+              }
+            }}
+          >
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
