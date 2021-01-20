267858059470da7ceeb838b0eb73e3bfed720877
danst3in <50937807+danst3in@users.noreply.github.com>
2021-01-20 11:25:57 -0800

    Signout mutation, component,button font fix

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -93,6 +93,10 @@ const Mutations = {
     //  5. return user
     return user;
   },
+  async signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Goodbye!" };
+  },
 };
 
 module.exports = Mutations;
