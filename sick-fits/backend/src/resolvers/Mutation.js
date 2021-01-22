40ed56bde6780dbbe56156822461112991643a3f
danst3in <50937807+danst3in@users.noreply.github.com>
2021-01-21 21:30:58 -0800

    AddToCart BE and FE  buttons/component

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -208,6 +208,43 @@ const Mutations = {
       info
     );
   },
+  async addToCart(parent, args, ctx, info) {
+    // check if logged in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error("You must be signed in");
+    }
+    // Query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      userId: { id: userId },
+      item: { id: args.id },
+    });
+    // check if item is already in the cart - increment or ...
+    if (existingCartItem) {
+      console.log("This item is already in their cart!");
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 },
+        },
+        info
+      );
+    }
+    // add new item to cart
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: { id: userId },
+          },
+          item: {
+            connect: { id: args.id },
+          },
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
