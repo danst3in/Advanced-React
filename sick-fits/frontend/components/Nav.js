865b768c2174e5037932bdab94794bf1ac201e42
danst3in <50937807+danst3in@users.noreply.github.com>
2021-01-22 15:26:05 -0800

    Animate Cart Count Component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Mutation } from "react-apollo";
 import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCount from "./CartCount";
 import Signout from "./Signout";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
@@ -25,7 +26,16 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {(toggleCart) => <button onClick={toggleCart}>My Cart</button>}
+              {(toggleCart) => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce((tally, cartItems) => {
+                      return tally + cartItems.quantity;
+                    }, 0)}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
